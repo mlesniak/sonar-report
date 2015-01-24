83a3f5961645dae8e106dc82a01a2a64699e003c
Michael Lesniak <mlesniak@micromata.de>
2015-01-24 15:42:25 +0100

    Assembly plugin configured.

---
+++ src/main/java/com/mlesniak/sonar/report/Main.java
@@ -0,0 +1,12 @@
+package com.mlesniak.sonar.report;
+
+/**
+ * Main entry point.
+ *
+ * @author Michael Lesniak (mail@mlesniak.com)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
